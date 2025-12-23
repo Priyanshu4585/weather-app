838a7a6e5eadb96cec0056368e597669d6654211
Priyanshu Singh <152967318+Priyanshu4585@users.noreply.github.com>
2025-12-23 14:52:55 +0530

    Updated site

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -74,8 +74,6 @@ useEffect(() => {search("delhi")}, []);
 
   return (
     <div className="weather">
-                <div className="weather-bubbles" id="bubbles"><div className="bubble" style={{width: "66px", height: "66px", left: "22%", top: "76%", animationDuration: "9s", animationDelay: "4s"}}></div><div className="bubble" style={{width: "58px", height: "58px", left: "71%", top: "8%", animationDuration: "11s", animationDelay: "3s"}}></div><div className="bubble" style={{width: "47px", height: "47px", left: "91%", top: "64%", animationDuration: "6s", animationDelay: "1s"}}></div><div className="bubble" style={{width: "75px", height: "75px", left: "22%", top: "7%", animationDuration: "6s", animationDelay: "2s"}}></div><div className="bubble" style={{width: "79px", height: "79px", left: "18%", top: "91%", animationDuration: "8s", animationDelay: "1s"}}></div><div className="bubble" style={{width: "56px", height: "56px", left:"33%", top:"40%", animationDuration:"7s", animationDelay:"3s"}}></div><div className="bubble" style={{width:"22px", height:"22px", left:"44%", top:"92%", animationDuration:"11s", animationDelay:"1s"}}></div><div className="bubble" style={{width:"46px", height:"46px", left:"18%", top:"28%", animationDuration:"6s", animationDelay:"3s"}}></div></div>
-
       <div className="search-bar">
         <input
           ref={inputRef}
